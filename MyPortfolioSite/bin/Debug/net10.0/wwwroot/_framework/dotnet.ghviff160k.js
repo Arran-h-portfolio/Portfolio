3625bac08a046eda28dd69fdef68940513789f3b
BVB-Glitch <207627199+Arran-h-portfolio@users.noreply.github.com>
2026-06-03 19:07:59 +0100

    Added more badges to home page and updated my tech stack

--- MyPortfolioSite/bin/Debug/net10.0/wwwroot/_framework/dotnet.ghviff160k.js
+++ MyPortfolioSite/bin/Debug/net10.0/wwwroot/_framework/dotnet.ghviff160k.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "MyPortfolioSite",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-IGnnQqn+UY1gOIda6d8xSzi2V5CtBZa0QgEek0RArsI=",
+    "hash": "sha256-nsnHTzXtnrdNsZS0+VCms5aqh2PBnIIoQNsJcpLOReA=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.mx9wzm9o5h.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "MyPortfolioSite.wasm",
-        "name": "MyPortfolioSite.yhoo339xv7.wasm",
-        "integrity": "sha256-+s4Yr1AZ9FuBwpaNU2IIO4I/oeVSlnVwwox7Ks/wV4Q=",
+        "name": "MyPortfolioSite.2zm8p7yuu4.wasm",
+        "integrity": "sha256-TvtKlYA9am9qRhxmiWtaDGauvG/GR5O6X/iByUvle6Y=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "MyPortfolioSite.pdb",
-        "name": "MyPortfolioSite.m0n4n9r7p0.pdb",
-        "integrity": "sha256-hviT5Sw2t2omDVEEXRlrq5wQ3m8hryIborS4bz5hutM=",
+        "name": "MyPortfolioSite.9jz438tesr.pdb",
+        "integrity": "sha256-GyDWlwCl+KHFpiQwsZHkOy22C4piCJf/CznS2HTLFrQ=",
         "cache": "force-cache"
       }
     ],
--- MyPortfolioSite/obj/Debug/net10.0/MyPortfolioSite.AssemblyInfo.cs
+++ MyPortfolioSite/obj/Debug/net10.0/MyPortfolioSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolioSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7bb29eee15c2ede53595c282d9b91ec768d2e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42b20624779f3972325fb91b15eba489d80cbb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolioSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolioSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
