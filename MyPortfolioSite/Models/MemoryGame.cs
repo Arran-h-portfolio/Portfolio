1fab0e3c9c4fe183ca05c07e1b730320c11dfbf4
Arran Hewitson <AAAHStudios@outlook.com>
2026-06-05 07:53:34 +0100

    Began implementing logic for typing game by populating dictionarys for each round

--- MyPortfolioSite/Models/MemoryGame.cs
+++ MyPortfolioSite/Models/MemoryGame.cs
@@ -8,6 +8,9 @@ public class MemoryGame
     public bool IsGameStarted { get; set; }
     public int CorrectSentencesTyped { get; set; }
     public int TotalSentences { get; set; }
+    // Setting up rounds system
+    public int CurrentRound { get; set; } = 1;
+    public Dictionary<int, Dictionary<int, string>>? Rounds { get; set; }
     public Dictionary<int, string>? Sentences { get; set; }
     public int Score { get; set; }
     public int HighScore { get; set; }
